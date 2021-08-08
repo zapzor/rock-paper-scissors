2c218e176ba761dfd6d2ec03ee2129f9ac78d266
Zapzor <imp.4xlbm@aleeas.com>
2021-08-08 19:28:48 +0100

    Completed function that plays round

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,11 +1,29 @@
 //Create function that randomly returns rock, paper or scissors.
 function computerPlay() {
-
+    let game = ['Rock', 'Paper', 'Scissors'];
+    let rps = game[Math.floor(Math.random()*game.length)];
+    return rps;
 }
 //Create function that takes player input of rock, paper or scissors.
+function playerSelection() {
+    let lower = prompt('Rock, paper or scissors?', '')
+    let player = lower.toLowerCase
+}
 
 //Create function that plays a round of rock paper scissors.
-
+function playRound() {
+    let computer = computerPlay
+    if(lower === 'Rock' && computer === 'Paper') {
+        alert('You Win!')
+    }else if(lower === 'Scissors' && computer === 'Paper'){
+        alert('You win!')
+    }else if(lower === 'Paper' && computer === 'Rock'){
+            alert('You win!')
+    }else{
+        alert('You lose!')
+    }
+} 
+console.log(computerPlay);
 //Create function that declares winner of round.
 
 //Create function that plays 5 rounds of rock paper scissors and declares the winner with most won rounds.
