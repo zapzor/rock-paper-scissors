69f93f70d5054862d50d201ddcccb0819cf11dd5
Zapzor <imp.4xlbm@aleeas.com>
2021-08-08 17:38:10 +0100

    Pseudocode

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,3 +1,11 @@
+//Create function that randomly returns rock, paper or scissors.
 function computerPlay() {
-    
+
 }
+//Create function that takes player input of rock, paper or scissors.
+
+//Create function that plays a round of rock paper scissors.
+
+//Create function that declares winner of round.
+
+//Create function that plays 5 rounds of rock paper scissors and declares the winner with most won rounds.
