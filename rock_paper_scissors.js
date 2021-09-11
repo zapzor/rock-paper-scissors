9fbaccf0afa99dd7a76c319ae40a05aeedea2e6c
Zapzor <imp.4xlbm@aleeas.com>
2021-09-11 21:58:51 +0100

    Refactored code. Buttons now randomize correctly

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,59 +1,48 @@
-//html variables and score tracking
+// Score tracking and DOM containers
+
 let playerScore = 0
 let computerScore = 0
-const computerSelection = computerPlay();
+
 const container0 = document.querySelector(".div");
 const content = document.createElement("div");
-const score = document.createElement("p1");
+const score = document.createElement("div");
 
 container0.appendChild(content);
 container0.appendChild(score);
 
-rock.addEventListener("click", function () {
-    playerSelection = "rock"
-    console.log(game(playerSelection, computerSelection))
-})
+// Function that randomly returns rock, paper or scissors
 
-paper.addEventListener("click", function () {
-    playerSelection = "paper"
-    console.log(game(playerSelection, computerSelection))
-})
-
-scissors.addEventListener("click", function () {
-    playerSelection = "scissors"
-    console.log(game(playerSelection, computerSelection))
-})
-
-//Function that randomly returns rock, paper or scissors.
 function computerPlay() {
-    let computerChoice = ["rock", "paper", "scissors"]
-    let computerPlay = computerChoice[Math.floor(Math.random()*computerChoice.length)];
-    return computerPlay;
+  let computerChoice = ["rock", "paper", "scissors"]
+  return computerChoice[Math.floor(Math.random() * 3)];
 }
 
-//Function that plays a round of RPS.
+// Function that plays a round of RPS
+
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     content.textContent = "Draw"
       return "Draw";
   }else if (playerSelection === "scissors" && computerSelection === "paper"
-    || playerSelection === "paper" && computerSelection === "rock"
-    || playerSelection === "rock" && computerSelection === "scissors") {
+   || playerSelection === "paper" && computerSelection === "rock"
+   || playerSelection === "rock" && computerSelection === "scissors") {
       playerScore++
       content.textContent = "You win!"
-      score.textContent = `Player score is ${playerScore}, computer score is ${computerScore}`;
+      score.textContent = `Player score is ${playerScore}, computer score is ${computerScore}`
   }else if (playerSelection === "rock" && computerSelection === "paper"
     || playerSelection === "scissors" && computerSelection === "rock"
     || playerSelection === "paper" && computerSelection === "scissors") {
       computerScore++
-      content.textContent = "You lose!";
-      score.textContent = `Player score is ${playerScore}, computer score is ${computerScore}`;
+      content.textContent = "You lose!"
+      score.textContent = `Player score is ${playerScore}, computer score is ${computerScore}`
       return "You lose!";
   }else {
     content.textContent = "Something went wrong";
     return "Something went wrong";
-      }
+  }
 }
+
+// Functions that plays 5 rounds of RPS
 
 function game() {
   if (playerScore || computerScore != 5) {
@@ -71,3 +60,19 @@ function game() {
     }
   }
 
+// Event listeners for buttons
+
+  rock.addEventListener("click", function() {
+    playerSelection = "rock"
+    console.log(playRound(playerSelection, computerPlay()))
+  })
+
+  paper.addEventListener("click", function () {
+    playerSelection = "paper"
+    console.log(playRound(playerSelection, computerPlay()))
+  })
+
+  scissors.addEventListener("click", function () {
+    playerSelection = "scissors"
+    console.log(playRound(playerSelection, computerPlay()))
+  })
